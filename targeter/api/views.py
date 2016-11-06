1c5405eae75e77588885fb858ef05d7d73dabdb7
Artem Innokentiev <artinnok@mail.ru>
2016-11-06 13:50:29 +0300

    ADD successful OAuth

--- targeter/api/views.py
+++ targeter/api/views.py
@@ -9,37 +9,34 @@ class AuthorizeView(APIView):
     """
     Авторизует пользователя в приложении
     """
-    url = ('https://oauth.vk.com/authorize?'
+    url = ('https://api.instagram.com/oauth/authorize/?'
            'client_id={client_id}&'
-           'display=page&'
            'redirect_uri={redirect_uri}&'
-           'scope={scope}&'
-           'response_type=token&'
-           'v=5.59')
+           'response_type=code&'
+           'scope={scope}')
 
     def get(self, request, *args, **kwargs):
         return redirect(self.url.format(
             client_id=settings.CLIENT_ID,
             redirect_uri=settings.REDIRECT_URI,
-            scope='wall,offline'
+            scope='public_content likes'
         ))
 
 
 class CallbackView(APIView):
-    url = 'https://oauth.vk.com/access_token?client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&code={code}'
+    """
+    Сюда придет коллбэк от Инстаграма
+    """
+    url = 'https://api.instagram.com/oauth/access_token'
 
     def get(self, request, *args, **kwargs):
         code = request.query_params['code']
-        r = requests.get(self.url.format(
-            client_id=settings.CLIENT_ID,
-            client_secret=settings.CLIENT_SECRET,
-            redirect_uri=settings.REDIRECT_URI,
-            code=code
-        ))
-        json = r.json()
-        Token.objects.update_or_create(
-            access_token=json['access_token'],
-            defaults=json
-        )
-        return Response('ok')
+        json = requests.post(self.url, data={
+            'client_id': settings.CLIENT_ID,
+            'client_secret': settings.CLIENT_SECRET,
+            'redirect_uri': settings.REDIRECT_URI,
+            'code': code,
+            'grant_type': 'authorization_code',
+        }).json()
+        return Response(json)
 
--- targeter/config/settings/instagram.py
+++ targeter/config/settings/instagram.py
@@ -3,3 +3,5 @@ import os
 CLIENT_ID = os.environ['CLIENT_ID']
 
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
+
+REDIRECT_URI = 'http://localhost:8000/api/callback/'
