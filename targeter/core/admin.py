f58f131b35fb7594828e2904f1ae3f4e62db527c
Artem Innokentiev <artinnok@mail.ru>
2016-11-06 13:56:58 +0300

    ADD User model

--- targeter/core/admin.py
+++ targeter/core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth import models
 
+from core.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'username', 'access_token')
+
+admin.site.register(User, UserAdmin)
+
 
 # django user and group
 admin.site.unregister(models.User)
--- targeter/core/models.py
+++ targeter/core/models.py
@@ -4,3 +4,28 @@ from core import common_models as cm
 from core import behaviors as bh
 
 
+class User(cm.Common):
+    """
+    Пользователь Инстаграма
+    """
+    user_id = models.BigIntegerField(
+        verbose_name='ID пользователя',
+        unique=True
+    )
+    username = models.CharField(
+        max_length=200,
+        verbose_name='Никнейм',
+        unique=True
+    )
+    access_token = models.CharField(
+        max_length=200,
+        verbose_name='Токен'
+    )
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.user_id)
